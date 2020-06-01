1d17e09b3da3cc2ba69706db5aa35970fa24d540
Fatin Ihsan <ihsanfatin3@gmail.com>
2020-06-01 12:02:19 +0600

    add heroku settings

--- BlogProject/BlogProject/settings.py
+++ BlogProject/BlogProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
+
+django_heroku.settings(locals())
